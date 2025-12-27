56ca5dbcfbadc5d99200ea15fd5d884d830fc41a
Vapos <Vaposs@bk.ru>
2025-12-27 16:54:42 +0300

    addStartLogerScript

---
+++ StartLoger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ZD_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class ConsoleLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+}
